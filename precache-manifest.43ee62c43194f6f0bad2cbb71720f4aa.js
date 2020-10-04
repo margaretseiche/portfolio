1547c0e3d12febd9b8eeebf8d26a43fea28531e9
Margaret Seiche <margaretseiche@hotmail.com>
2020-10-04 17:46:54 -0400

    Updates

---
+++ precache-manifest.43ee62c43194f6f0bad2cbb71720f4aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e66585ff64ebd93a53d4f5a7842d9e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1a98e1a10f500d3e4d06",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "de2483aa3730a7590e06",
+    "url": "/portfolio/static/css/main.5c392709.chunk.css"
+  },
+  {
+    "revision": "1a98e1a10f500d3e4d06",
+    "url": "/portfolio/static/js/2.27b4084a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.27b4084a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2483aa3730a7590e06",
+    "url": "/portfolio/static/js/main.6fd2a54b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
